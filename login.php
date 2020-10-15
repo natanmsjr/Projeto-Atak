05a118d6482275aea160078fead325042f59e204
Natanael <natan.msjr@gmail.com>
2020-10-14 22:25:05 -0300

    base do layout da págian de login e css

---
+++ login.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="ie8 no-js" lang="pt-br"><![endif]-->
+<!--[if IE 9]><html class="ie9 no-js" lang="pt-br"><![endif]-->
+<!--[if !IE]><!-->
+<!--<![endif]-->
+<html lang="pt-br" class="no-js">
+    <head>
+        <!-- Meta tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Language" content="pt-br">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="Natanael Macedo">
+        <meta name="description" content="Projeto Atak">
+        <!-- Título -->
+        <title>Projeto Atak</title>
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon/favicon.png">
+        <!-- CSS -->
+        <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <link href="assets/css/bootstrap-grid.min.css" rel="stylesheet" />
+        <link href="assets/css/argon-design-system.min.css" rel="stylesheet" />
+        <link href="assets/css/theme.css" rel="stylesheet" />
+    </head>
+    <body class="bg-light">
+        <!-- Page Content -->
+        <div class="container">
+            <div class="row shadow-sm my-lg-5">
+                <div class="col-12 col-lg-6 bg-gradient-default">
+                    <div class="row my-6 my-lg-8">
+                        <div class="col-12 text-center">
+                            <img class="img-fluid" src="assets/img/brand/EasyPAC.png" style="height: 45px;" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 bg-white">
+                    <div class="row mt-8">
+                        <div class="col-12 offset-lg-2 col-lg-8 text-center mb-3">
+                            <h5 class="text-uppercase text-login">Easypac</h5>
+                            <small class="text-muted">Bem-vindo! Faça o login em sua conta.</small>
+                        </div>
+                    </div>
+                    <div class="row mb-8">
+                        <div class="col-12 offset-lg-2 col-lg-8">
+                            <form id="form-login">
+                                <div id="div-feedback-form" class="form-group">
+                                    <small id="formHelp" class="form-text text-danger"><i></i></small>
+                                </div>
+                                <div id="div-input-email" class="form-group">
+                                    <input type="email" class="form-control" id="email" name="email" required />
+                                    <label for="email">E-mail</label>
+                                </div>
+                                <div id="div-input-password" class="form-group">
+                                    <input type="password" class="form-control" id="password" name="password" required />
+                                    <label for="password">Senha</label>
+                                </div>
+                                <div class="clearfix">
+                                    <div id="div-input-remember" class="form-group custom-control custom-checkbox d-inline float-left">
+                                        <input type="checkbox" class="custom-control-input" id="remember" name="remember" />
+                                        <label class="custom-control-label" for="remember">Manter conectado</label>
+                                    </div>
+                                    <div id="div-link-forgot" class="form-group d-inline float-right text-right">
+                                        <label><a class="text-reset" href="#">Esqueceu a senha?</a></label>
+                                    </div>
+                                </div>
+                                <div class="text-center mt-3">
+                                    <button type="submit" class="btn btn-default px-5">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 text-center mb-3">
+                            <small><a class="text-reset" href="#"><sub>Termos de Uso.</sub></a> <a class="text-reset" href="#"><sub>Política de Privacidade.</sub></a></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Required scripts -->
+        <scripts>
+            <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
+            <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+        </scripts>
+    </body>
+</html>
